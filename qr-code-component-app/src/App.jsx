f1bde5e4493546f10e30eb31a6c2929d9f22d36f
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-03-26 14:09:54 -0400

    initial commit

---
+++ qr-code-component-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+	return (
+		<>
+			<div className="text-3xl font-bold underline">
+				<h1>Hello World</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
