0a884ffd2e37bad39441b8da7fcc35cabfc0ca73
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-03-26 14:28:34 -0400

    add html layout

--- qr-code-component-app/src/App.jsx
+++ qr-code-component-app/src/App.jsx
@@ -1,11 +1,24 @@
+import qrcodeImage from "./assets/images/image-qr-code.png";
 import "./index.css";
 
 function App() {
 	return (
 		<>
-			<div className="text-3xl font-bold underline">
-				<h1>Hello World</h1>
-			</div>
+			<main className="text-3xl">
+				<div className="image">
+					<img
+						src={qrcodeImage}
+						alt="qr code"
+					/>
+				</div>
+				<div className="message">
+					<h1>Improve your front-end skills by building projects</h1>
+					<p>
+						Scan the QR code to visit Frontend Mentor and take your coding
+						skills to the next level
+					</p>
+				</div>
+			</main>
 		</>
 	);
 }
