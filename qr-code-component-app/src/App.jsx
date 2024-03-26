8429e349dca84a0c20db431984284b9aa29cd7b1
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-03-26 15:05:28 -0400

    add style

--- qr-code-component-app/src/App.jsx
+++ qr-code-component-app/src/App.jsx
@@ -1,19 +1,24 @@
 import qrcodeImage from "./assets/images/image-qr-code.png";
 import "./index.css";
+import "./App.css";
 
 function App() {
 	return (
 		<>
-			<main className="text-3xl">
-				<div className="image">
+			<main className="rounded-3xl bg-white">
+				<div className="image rounded-lg">
 					<img
 						src={qrcodeImage}
 						alt="qr code"
+						height={288}
+						width={288}
 					/>
 				</div>
 				<div className="message">
-					<h1>Improve your front-end skills by building projects</h1>
-					<p>
+					<h1 className="text-center font-bold text-2xl">
+						Improve your front-end skills by building projects
+					</h1>
+					<p className="text-center">
 						Scan the QR code to visit Frontend Mentor and take your coding
 						skills to the next level
 					</p>
