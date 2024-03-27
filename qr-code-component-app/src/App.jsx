9ba0d9fae27b3c2729bb1ec3ba849887ed54c61e
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-03-27 11:39:21 -0400

    add style class to element

--- qr-code-component-app/src/App.jsx
+++ qr-code-component-app/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 	return (
 		<>
 			<main className="rounded-3xl bg-white max-w-80 max-h-fit p-5">
-				<div>
+				<div className="pb-5">
 					<img
 						className="rounded-lg"
 						src={qrcodeImage}
@@ -16,10 +16,10 @@ function App() {
 					/>
 				</div>
 				<div className="message">
-					<h1 className="text-center font-bold">
+					<h1 className="text-center text-dark-navy font-bold text-lg px-5">
 						Improve your front-end skills by building projects
 					</h1>
-					<p className="text-center">
+					<p className="text-center text-grayish-blue font-normal text-sm p-5">
 						Scan the QR code to visit Frontend Mentor and take your coding
 						skills to the next level
 					</p>
