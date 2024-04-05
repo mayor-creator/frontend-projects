166031022cb83611ee2b665c497ddfb5e75bef1b
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-04-05 06:43:18 -0400

    add fetch api call

--- advice-generator-app/src/Advice.jsx
+++ advice-generator-app/src/Advice.jsx
@@ -2,17 +2,52 @@ import icon from "./assets/images/icon-dice.svg";
 import mobile from "./assets/images/pattern-divider-mobile.svg";
 import desktop from "./assets/images/pattern-divider-desktop.svg";
 
+import { useState, useEffect } from "react";
+
 export function Advice() {
+	const [data, setData] = useState({});
+	const [counter, setCounter] = useState(0);
+	const url = "https://api.adviceslip.com/advice";
+
+	useEffect(() => {
+		fetch(url)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(`${response.status}`);
+				}
+				return response.json();
+			})
+			.then((fetchData) => {
+				setData((previousData) => {
+					if (previousData != fetchData) {
+						return fetchData;
+					}
+					return previousData;
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, [counter]);
+
+	const handleClick = () => {
+		setCounter((previous) => previous + 1);
+	};
+
+	let objectItems = data;
+	const message = Object.values(objectItems).map((item) => {
+		return (
+			<>
+				<h1>Advice #{item.id}</h1>
+				<p>{item.advice}</p>
+			</>
+		);
+	});
+
 	return (
 		<>
 			<main className="container">
-				<section>
-					<h1 className="advice_n0">Advice #</h1>
-					<p className="message">
-						&quot;It is easy to sit up and take notice, What&apos;s difficult is
-						getting up and taking action.&quot;
-					</p>
-				</section>
+				<section>{message}</section>
 				<picture>
 					<source
 						srcSet={mobile}
@@ -28,7 +63,7 @@ export function Advice() {
 					/>
 				</picture>
 
-				<button>
+				<button onClick={handleClick}>
 					<img
 						src={icon}
 						aria-label="hidden"></img>
