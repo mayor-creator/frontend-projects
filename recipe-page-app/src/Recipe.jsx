8ae91193e4d8dc81ce98e935e32570308f08ac62
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-04-15 11:54:16 -0400

    add class name to ingredients list

--- recipe-page-app/src/Recipe.jsx
+++ recipe-page-app/src/Recipe.jsx
@@ -65,31 +65,31 @@ export function Recipe() {
 							<ol
 								type="1"
 								className="instructions-list">
-								<li>
+								<li className="instructions-items">
 									<span>Beat the eggs</span>: In a bowl, beat the eggs with a
 									pinch of salt and pepper until they are well mixed. You can
 									add a tablespoon of water or milk for a fluffier texture.
 								</li>
-								<li>
+								<li className="instructions-items">
 									<span>Heat the pan</span>: Place a non-stick frying pan over
 									medium heat and add butter or oil.
 								</li>
-								<li>
+								<li className="instructions-items">
 									<span>Cook the omelette</span>: Once the butter is melted and
 									bubbling, pour in the eggs. Tilt the pan to ensure the eggs
 									evenly coat the surface.
 								</li>
-								<li>
+								<li className="instructions-items">
 									<span>Add fillings (optional)</span>: When the eggs begin to
 									set at the edges but are still slightly runny in the middle,
 									sprinkle your chosen fillings over one half of the omelette.
 								</li>
-								<li>
+								<li className="instructions-items">
 									<span>Fold and serve</span>: As the omelette continues to
 									cook, carefully lift one edge and fold it over the fillings.
 									Let it cook for another minute, then slide it onto a plate.
 								</li>
-								<li>
+								<li className="instructions-items">
 									<span>Enjoy</span>: Serve hot, with additional salt and pepper
 									if needed.
 								</li>
