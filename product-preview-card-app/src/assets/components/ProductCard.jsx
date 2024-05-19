8c75c83e402861b210ac6c37338f72f299fe18e0
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-05-18 20:39:44 -0400

    add product component information

--- product-preview-card-app/src/assets/components/ProductCard.jsx
+++ product-preview-card-app/src/assets/components/ProductCard.jsx
@@ -1,3 +1,19 @@
+import { ImageCard } from "./ImageCard";
+import { ProductDescriptionCard } from "./ProductDescriptionCard";
+
 export function ProductCard() {
-	return <></>;
+	return (
+		<>
+			<div className="wrapper">
+				<main className="main">
+					<div className="main__image--container">
+						<ImageCard />
+					</div>
+					<div className="main__product--container">
+						<ProductDescriptionCard />
+					</div>
+				</main>
+			</div>
+		</>
+	);
 }
