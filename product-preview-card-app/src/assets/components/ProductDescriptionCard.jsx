3b7c7b64679beb366f91b26a55fd2841bcd7dd4a
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-05-20 21:02:10 -0400

    add item class name

--- product-preview-card-app/src/assets/components/ProductDescriptionCard.jsx
+++ product-preview-card-app/src/assets/components/ProductDescriptionCard.jsx
@@ -3,14 +3,18 @@ import iconCart from "../../images/icon-cart.svg";
 export function ProductDescriptionCard() {
 	return (
 		<>
-			<p className="main__product--header">Perfume</p>
-			<h1 className="main__product--name">Gabrielle Essence Eau De Parfum</h1>
-			<p className="main__product--description">
+			<p className="main__product--header item">Perfume</p>
+			<h1 className="main__product--name item">
+				Gabrielle Essence Eau De Parfum
+			</h1>
+			<p className="main__product--description item">
 				A floral, solar and voluptuous interpretation composed by Olivier Polge,
 				Perfumer-Creator for the House of CHANEL.
 			</p>
-			<p className="main__product--price"> $149.99</p>
-			<span className="main__product--discount"> $169.99</span>
+			<div className="main__product--price-container">
+				<p className="main__product--price item"> $149.99</p>
+				<p className="main__product--discount item"> $169.99</p>
+			</div>
 			<button className="main__product--button">
 				<img
 					className="main__product--button-image"
