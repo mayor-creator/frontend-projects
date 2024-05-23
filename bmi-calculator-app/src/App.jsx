b666b7b37d4b81b2f6feaa62e4f62a37eb5cdb89
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-05-23 07:26:19 -0400

    change component directory

--- bmi-calculator-app/src/App.jsx
+++ bmi-calculator-app/src/App.jsx
@@ -1,17 +1,10 @@
 import { Hero } from "./components/Hero";
-import { HealthTips } from "./components/HealthTips";
-import { BMIFactor } from "./components/BMIFactor";
-
 import "./sass/styles.scss";
 
 function App() {
 	return (
 		<>
 			<Hero />
-			<main>
-				<HealthTips />
-				<BMIFactor />
-			</main>
 		</>
 	);
 }
