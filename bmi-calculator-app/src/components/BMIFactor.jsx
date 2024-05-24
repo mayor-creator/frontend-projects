0ea56d8ec066eac0a7720402b6dd9d675fc6775f
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-05-24 12:52:10 -0400

    add class names to BMIFactor and HealthTips components

--- bmi-calculator-app/src/components/BMIFactor.jsx
+++ bmi-calculator-app/src/components/BMIFactor.jsx
@@ -7,89 +7,96 @@ import raceIcon from "../assets/images/icon-race.svg";
 export function BMIFactor() {
 	return (
 		<>
-			<section>
-				<div>
-					<h2>Limitations of BMI</h2>
-					<p>
+			<section className="main__bmi-facts wrapper-bmi-facts">
+				<div className="main__bmi-facts-limitations">
+					<h2 className="main__bmi-facts-limitations-header">
+						Limitations of BMI
+					</h2>
+					<p className="main__bmi-facts-limitations-info">
 						Although BMI is often a practical indicator of healthy weight, it is
 						not suited for every person. Specific groups should carefully
 						consider their BMI outcomes, and in certain cases, the measurement
 						may not be beneficial to use.
 					</p>
 				</div>
-				<div>
-					<div>
+				<div className="main__bmi-facts-gender">
+					<div className="main__item">
 						<img
+							className="main__bmi-facts-gender-image"
 							src={genderIcon}
 							alt="gender icon"
 							width="32"
 							height="32"
 						/>
-						<h4>Gender</h4>
+						<h4 className="main__bmi-facts-gender-header">Gender</h4>
 					</div>
-					<p>
+					<p className="main__bmi-facts-gender-info">
 						The development and body fat composition of girls and boys vary with
 						age. Consequently, a child&apos;s age and gender are considered when
 						evaluating their BMI.
 					</p>
 				</div>
-				<div>
-					<div>
+				<div className="main__bmi-facts-age">
+					<div className="main__item">
 						<img
+							className="main__bmi-facts-age-image"
 							src={ageIcon}
 							alt="age icon"
 							width="32"
 							height="32"
 						/>
-						<h4>Age</h4>
+						<h4 className="main__bmi-facts-age-header">Age</h4>
 					</div>
-					<p>
+					<p className="main__bmi-facts-age-info">
 						In aging individuals, increased body fat and muscle loss may cause
 						BMI to underestimate body fat content.
 					</p>
 				</div>
-				<div>
-					<div>
+				<div className="main__bmi-facts-muscle">
+					<div className="main__item">
 						<img
+							className="main__bmi-facts-muscle-image"
 							src={muscleIcon}
 							alt="muscle icon"
 							width="32"
 							height="32"
 						/>
-						<h4>Muscle</h4>
+						<h4 className="main__bmi-facts-muscle-header">Muscle</h4>
 					</div>
-					<p>
+					<p className="main__bmi-facts-muscle-info">
 						BMI may misclassify muscular individuals as overweight or obese, as
 						it doesn&apos;t differentiate muscle from fat.
 					</p>
 				</div>
-				<div>
-					<div>
+				<div className="main__bmi-facts-pregnancy">
+					<div className="main__item">
 						<img
+							className="main__bmi-facts-pregnancy-image"
 							src={pregnancyIcon}
 							alt="pregnancy icon"
 							width="32"
 							height="32"
 						/>
-						<h4>Pregnancy</h4>
+						<h4 className="main__bmi-facts-pregnancy-header">Pregnancy</h4>
 					</div>
-					<p>
+					<p className="main__bmi-facts-pregnancy-info">
 						Expectant mothers experience weight gain due to their growing baby.
 						Maintaining a healthy pre-pregnancy BMI is advisable to minimise
 						health risks for both mother and child.
 					</p>
 				</div>
-				<div>
-					<div>
+				<div className="main__bmi-facts-race">
+					<div className="main__item">
 						<img
+							className="main__bmi-facts-race-image"
 							src={raceIcon}
 							alt="race icon"
 							width="32"
 							height="32"
 						/>
-						<h4>Race</h4>
+						<h4 className="main__bmi-facts-race-header">Race</h4>
 					</div>
-					<p>
+					<p className="main__bmi-facts-race-info">
 						Certain health concerns may affect individuals of some Black and
 						Asian origins at lower BMIs than others. To learn more, it is
 						advised to discuss this with your GP or practice nurse.
--- bmi-calculator-app/src/components/HealthTips.jsx
+++ bmi-calculator-app/src/components/HealthTips.jsx
@@ -4,42 +4,45 @@ import sleep from "../assets/images/icon-sleep.svg";
 export function HealthTips() {
 	return (
 		<>
-			<section>
-				<div>
-					<div>
-						<div>
-							<img
-								src={eatingIcon}
-								alt="food icon"
-							/>
-							<h3>Healthy eating</h3>
-							<p>
-								Healthy eating promotes weight control, disease prevention,
-								better digestion, immunity, mental clarity, and mood.
-							</p>
-						</div>
-						<div>
-							<img
-								src={exerciseIcon}
-								alt="weight icon"
-							/>
-							<h3>Regular exercise</h3>
-							<p>
-								Exercise improves fitness, aids weight control, elevates mood,
-								and reduces disease risk, fostering wellness and longevity.
-							</p>
-						</div>
-						<div>
-							<img
-								src={sleep}
-								alt="sleep icon"
-							/>
-							<h3>Adequate sleep</h3>
-							<p>
-								Sleep enhances mental clarity, emotional stability, and physical
-								wellness, promoting overall restoration and rejuvenation.
-							</p>
-						</div>
+			<section className="main__health-tips">
+				<div className="main__health-tips-container wrapper-health-tips">
+					<div className="main__health-tips-eating">
+						<img
+							className="main__health-tips-eating-image"
+							src={eatingIcon}
+							alt="food icon"
+						/>
+						<h3 className="main__health-tips-eating-header">Healthy eating</h3>
+						<p className="main__health-tips-eating-info">
+							Healthy eating promotes weight control, disease prevention, better
+							digestion, immunity, mental clarity, and mood.
+						</p>
+					</div>
+					<div className="main__health-tips-exercise">
+						<img
+							className="main__health-tips-exercise-image"
+							src={exerciseIcon}
+							alt="weight icon"
+						/>
+						<h3 className="main__health-tips-exercise-header">
+							Regular exercise
+						</h3>
+						<p className="main__health-tips-exercise-info">
+							Exercise improves fitness, aids weight control, elevates mood, and
+							reduces disease risk, fostering wellness and longevity.
+						</p>
+					</div>
+					<div className="main__health-tips-sleep">
+						<img
+							className="main__health-tips-sleep-image"
+							src={sleep}
+							alt="sleep icon"
+						/>
+						<h3 className="main__health-tips-sleep-header">Adequate sleep</h3>
+						<p className="main__health-tips-sleep-info">
+							Sleep enhances mental clarity, emotional stability, and physical
+							wellness, promoting overall restoration and rejuvenation.
+						</p>
 					</div>
 				</div>
 			</section>
